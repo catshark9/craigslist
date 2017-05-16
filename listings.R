14643ce5c2e2bee0197b8581936ee65462d35b0e
catshark9 <jonkelley2@icloud.com>
2017-05-16 13:38:18 -0500

    Update listings.R

--- listings.R
+++ listings.R
@@ -13,11 +13,45 @@ listings3 <- listings3[grepl('64', listings3$desc), ]
 listings <- rbind(listings1, listings2, listings3)              
 
 
-listings_prev <- read_csv("C:/Users/Jon Kelley/Desktop/CL/listings.csv")
-listings_all <- rbind(listings, listings_prev)
-listings_all <- listings_all[!duplicated(listings_all$url), ]
-
-write.csv(listings_all, file='listings.csv',  row.names=FALSE)
 
 
-listings_new <- listings[!(listings$url%in%listings_prev$url), ]
+
+
+
+
+
+
+if(database){
+  
+  library('RPostgreSQL')
+  pg = dbDriver("PostgreSQL")
+  con = dbConnect(pg, user="user", password="password",
+                  host="localhost", port=5432, dbname="Craigslist")
+  listings_prev <- dbGetQuery(con, "SELECT * from listings")
+  
+  
+  
+  listings_all <- rbind(listings, listings_prev)
+  listings_all <- listings_all[!duplicated(listings_all$url), ]
+  
+  listings_new <- listings[!(listings$url%in%listings_prev$url), ]
+  
+  dbRemoveTable(con,"listings_new")
+  dbWriteTable(con,'listings_new', listings_new, row.names=FALSE)
+  
+  dbWriteTable(con, 'listings', value=listings_new,append=TRUE, row.names=FALSE)
+  
+
+} else {
+  listings_prev <- read_csv("C:/Users/Jon Kelley/Desktop/CL/listings.csv")
+  
+  listings_all <- rbind(listings, listings_prev)
+  listings_all <- listings_all[!duplicated(listings_all$url), ]
+  
+  write.csv(listings_all, file='listings.csv',  row.names=FALSE)
+  
+  listings_new <- listings[!(listings$url%in%listings_prev$url), ]
+}
+
+
+
